8320670e6b22517cda697461327ef505293dbb85
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-12-09 01:37:23 +0900

    Added a simple search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
+        <div className="App-header">logo</div>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by me</footer>
+      </div>
       <header className="App-header"></header>
     </div>
   );
 }
-
-export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function keywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={keywordChange} autoFocus="on" />
+      </form>
+    </div>
+  );
+}
