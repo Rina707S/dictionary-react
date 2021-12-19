a822da41a216fa2c335203dbe72d71b821ec4678
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-12-19 22:46:01 +0900

    Added and styled footer

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="App-header">logo</div>
+        <div className="App-header"></div>
         <main>
           <Dictionary />
         </main>
-        <footer>Coded by me</footer>
+        <footer>
+          This app was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ekaterina-romanenkova-9689b4211/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rina R
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/Rina707S/dictionary-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on github
+          </a>
+        </footer>
       </div>
       <header className="App-header"></header>
     </div>
