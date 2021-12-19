c9884b7e624d14662a478f09b0b0d7d18cd2d02e
Rina707S <88797166+Rina707S@users.noreply.github.com>
2021-12-19 13:34:16 +0900

    Added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleImages(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +22,12 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001e3c5333a4ae84eb39aa885e4f26f14d9";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=16`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handleImages);
   }
 
   function keywordChange(event) {
@@ -37,6 +49,7 @@ export default function Dictionary() {
         <div className="hint">Suggested words: moon, night, book etc...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
